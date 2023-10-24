2fd5c34e2103c5cafb3cf4792aca008220eb4723
Scapsters <scotty.happy@gmail.com>
2023-10-24 16:19:25 -0400

    nil will concatanate without error in debug mode

--- constants.lua
+++ constants.lua
@@ -1,3 +1,9 @@
 -- Most files will require this class
 
 DEBUG = true
+if DEBUG then
+    debug.setmetatable(nil, {
+        __tostring = function() return "nil" end,
+        __concat = function() return "nil" end
+    })
+end
