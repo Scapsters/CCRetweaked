873fe3bf65a509c2b1e844854b512cb01ff36eaf
Scapsters <scotty.happy@gmail.com>
2023-10-24 13:10:18 -0400

    add test names, print things when tests pass
    and add a basic test

--- proper_tests.lua
+++ proper_tests.lua
@@ -1,3 +1,5 @@
+require("world_stack")
+
 local Tester = {
     _tests = {n = 0},
 
@@ -15,13 +17,15 @@ local Tester = {
             if i == "n" then return end -- Dont call `run` on the integer...
             io.write(i..": ")
             test:run()
+            io.write("\n")
         end
     end
 }
 
 Test = {
-    new = function(self, testFunction, expectedValue, falseOutput)
+    new = function(self, testName, testFunction, expectedValue, falseOutput)
         local test = {
+            _testName = testName,
             _testFunction = testFunction,
             _expectedValue = expectedValue,
             _falseOutput = falseOutput
@@ -31,23 +35,34 @@ Test = {
     end,
 
     run = function(self)
+        io.write("["..self._testName.."] ")
         local result = self._testFunction()
         if result ~= self._expectedValue then
-            print(
+            io.write(
                 self._falseOutput.."\t\t"..
                 "Expected result: ".. self._expectedValue.."\t\t"..
                 "Actual result: ".. result
             )
+        else
+            io.write("Passed")
         end
     end
 }
 
 local test1 = Test:new(
+    "`getId` on `Block` object",
     function()
-        return 12
-    end, 15, "Test1 failed!")
+        local id = "4 big guys"
+
+        local block = Block:new(id)
+
+        local experimental = block:getId()
+
+        return experimental
+    end, "4 big guys", "Test1 failed!")
 
 local test2 = Test:new(
+    "Whats 9 + 10",
     function()
         return 9 + 10
     end, 21, "this fuckin test failed wat da hell"
