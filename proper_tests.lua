5e922c748654985cb7b78b9ce6d8729964b94870
Scapsters <scotty.happy@gmail.com>
2023-10-24 15:23:21 -0400

    add worldStack tests

--- proper_tests.lua
+++ proper_tests.lua
@@ -58,7 +58,7 @@ Error = {
     end,
 
     __tostring = function(self)
-        return self._description.."| Expected: "..self._expected.." Actual: "..self._actual
+        return self._description.." | Expected: "..self._expected.." Actual: "..self._actual
     end
 }
 
@@ -221,7 +221,174 @@ local stackTests = Test:new(
     end
 )
 
+local worldStackTests = Test:new(
+    "worldStackTests",
+    function()
+        local id = "stella arcanum"
+        local block = Block:new(id)
+        local stack = Stack:new(block, 32)
+        local worldStack = WorldStack:new(stack)
+
+        local expected = 300
+        local actual = worldStack:getMaxAge()
+        if actual ~= expected then
+            return Error:new(expected, actual, "maxAge isnt set properly by default")
+        end
+
+        local worldStack = WorldStack:new(stack, 10)
+
+        local expected = 10
+        local actual = worldStack:getMaxAge()
+        if actual ~= expected then
+            return Error:new(expected, actual, "maxAge isnt set properly by argument")
+        end
+
+        local expected = id
+        local actual = worldStack:getStack():getId()
+        if actual ~= expected then
+            return Error:new(expected, actual, "getStack doesn't work")
+        end
+
+        local expected = id
+        local actual = worldStack:getStack():getBlock():getId()
+        if actual ~= expected then
+            return Error:new(expected, actual, "getStack then getBlock doesn't work somehow")
+        end
+
+        local expected = false
+        local actual = worldStack:shouldDespawn()
+        if actual ~= expected then
+            return Error:new(tostring(expected), tostring(actual), "shouldDespawn is wrong")
+        end
+
+        for i=1, 11 do worldStack:_tick() end
+
+        local expected = true
+        local actual = worldStack:shouldDespawn()
+        if actual ~= expected then
+            return Error:new(tostring(expected), tostring(actual), "shouldDespawn is wrong")
+        end
+    end
+)
+
+local worldStackInheritanceTests = Test:new(
+    "worldStackInheritanceTests",
+    function()
+        local id = "stella arcanum"
+        local block = Block:new(id)
+        local stack = Stack:new(block, 32)
+        local worldStack = WorldStack:new(stack, 10)
+
+        local expected = id
+        local actual = worldStack:getId()
+        if actual ~= expected then
+            return Error:new(expected, actual, "Id set wrong on creation")
+        end
+
+        local expected = 0
+        local actual = worldStack:getAge()
+        if actual ~= expected then
+            return Error:new(expected, actual, "Age set wrong on creation")
+        end
+
+        for i=1, 16 do worldStack:_tick() end
+
+        local expected = 16
+        local actual = worldStack:getAge()
+        if actual ~= expected then
+            return Error:new(expected, actual, "tick() didnt work")
+        end
+
+        worldStack:resetAge()
+        local expected = 0
+        local actual = worldStack:getAge()
+        if actual ~= expected then
+            return Error:new(expected, actual, "resetAge didnt work")
+        end
+
+        local expected = 32
+        local actual = worldStack:getNumber()
+        if actual ~= expected then
+            return Error:new(expected, actual, "age set wrong on creation")
+        end
+
+        local expected = id
+        local actual = worldStack:getBlock():getId()
+        if actual ~= expected then
+            return Error:new(expected, actual, "getBlock didn't work")
+        end
+
+        local expected = 0
+        local actual = worldStack:addItem(16)
+        if actual ~= expected then
+            return Error:new(expected, actual, "addItem didn't return properly (partial fill case)")
+        end
+
+        local expected = 48
+        local actual = worldStack:getNumber()
+        if actual ~= expected then
+            return Error:new(expected, actual, "addItem didn't add to number")
+        end
+
+        local expected = 16
+        local actual = worldStack:addItem(32)
+        if actual ~= expected then
+            return Error:new(expected, actual, "addItem didn't return properly (overfill case)")
+        end
+
+        local expected = 64
+        local actual = worldStack:getNumber()
+        if actual ~= expected then
+            return Error:new(expected, actual, "addItem didn't add to number properly (overfill limit failed)")
+        end
+
+        local expected = 0
+        local actual = worldStack:addItem(0)
+        if actual ~= expected then
+            return Error:new(expected, actual, "addItem didn't return properly (0 case)")
+        end
+
+        local expected = 64
+        local actual = worldStack:getNumber()
+        if actual ~= expected then
+            return Error:new(expected, actual, "addItem changed number on 0 case while full")
+        end
+
+        local expected = 64
+        local actual = worldStack:takeItem(100)
+        if actual ~= expected then
+            return Error:new(expected, actual, "takeItem returned improperly (overtake case)")
+        end
+
+        local expected = 0
+        local actual = worldStack:getNumber()
+        if actual ~= expected then
+            return Error:new(expected, actual, "takeItem didnt empty stack on overtake case")
+        end
+
+        local expected = 0
+        local actual = worldStack:takeItem(10)
+        if actual ~= expected then
+            return Error:new(expected, actual, "take Item returned improperly (take on empty stack case)")
+        end
+
+        local expected = 0
+        local actual = worldStack:getNumber()
+        if actual ~= expected then
+            return Error:new(expected, actual, "number changed on takeItem on an empty stack")
+        end
+
+        local expected = true
+        local actual = worldStack:isEmpty()
+        if actual ~= expected then
+            return Error:new(expected, actual, "isEmpty is plain fucking wrong :sob:")
+        end
+    end
+)
+
 Tester:addTest(blockTests)
 Tester:addTest(stackInheritanceTests)
 Tester:addTest(stackTests)
+Tester:addTest(worldStackInheritanceTests)
+Tester:addTest(worldStackTests)
 Tester:runTests()
