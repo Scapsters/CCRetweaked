cdcb2d3c1155773d7afc08738b35f4410f8681fe
Scapsters <scotty.happy@gmail.com>
2023-11-01 12:49:50 -0400

    remove outdated/redundant comments

--- inventory.lua
+++ inventory.lua
@@ -61,14 +61,12 @@ Inventory._setStack = function(self, slot, stack) self._stacks[slot] = stack end
 ---@param stack any
 ---@return integer overfill How many items were not returned
 Inventory.pickUp = function(self, stack)
-    -- Returns how many items were placed into the inventory
     local targetID = stack:getId()
     local number = stack:getNumber()
 
     local stacks = self:_getStacks()
     for index, slot in pairs(stacks) do
         if slot:getNumber() == 0 then
-            -- Empty slot. Dump it in the first slot and return number
             self:_setStack(index, Stack:new(nil, nil, stack)) -- Dereference stack
             stack:takeItem(number) -- Empty the stack
             return number
